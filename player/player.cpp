8652829584c7a1611ce2c5b570d973dd8240cee8
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-06-15 22:38:55 +0800

    complex雛形、自動判斷此電腦能到第幾步
    將Req重新更名為Required

--- player/player.cpp
+++ player/player.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+// 4的指數
+const vector<unsigned long> FOUR{4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824, 4294967296, 17179869184, 68719476736, 274877906944, 1099511627776, 4398046511104, 17592186044416, 70368744177664, 281474976710656, 1125899906842624, 4503599627370496, 18014398509481984, 72057594037927936, 288230376151711744, 1152921504606846976, 4611686018427387904};
+
 enum MapObjs{
 	MINE = 'b',	//3 round
 	MUSHROOM = 'm',	//+1
@@ -49,8 +52,12 @@ private:
 	unsigned short _width;
 };
 
+inline double progTime(){
+	return clock() * 1.0 / CLOCKS_PER_SEC;
+}
+
 /* Return true if the test is passed */
-class Req{
+class Required{
 public:
 	static bool wall(const short &row, const short &col);
 	static bool opponent(const short &row, const short &col);
@@ -68,6 +75,34 @@ private:
 	bool (*_methods[3])(const short&, const short&) = {wall, opponent, outOfRange};
 };
 
+class Complex{
+public:
+	int all(const short &row, const short &col, const bool &getMax){
+		int ret = getMax ? INT_MIN : INT_MAX;
+		for(auto &i : _methods){
+			ret = getMax ? max((i)(row, col), ret) : min((i)(row, col), ret);
+			if(ret == INT_MAX && getMax){
+				return ret;
+			}else if(ret == INT_MIN && !getMax){
+				return ret;
+			}
+		}
+		return ret;
+	}
+private:
+	int (*_methods[0])(const short&, const short&) = {};
+};
+
+class Bot{
+public:
+	Bot(){
+		benchmark();
+	}
+	void benchmark();
+private:
+	unsigned short _maxDepth;
+};
+
 Map gameMap;
 enum MapObjs WHOAMI;
 unsigned short ROUND;
@@ -76,6 +111,7 @@ pair<int, int>SCORE;
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(0);
+	srand(time(nullptr));
 	cin >> ROUND;
 	gameMap.read();
 	cin >> SCORE.first >> SCORE.second;
@@ -87,14 +123,29 @@ int main(){
 	return 0;
 }
 
-bool Req::wall(const short &row, const short &col){
+bool Required::wall(const short &row, const short &col){
 	return gameMap[row][col] != WALL;
 }
 
-bool Req::opponent(const short &row, const short &col){
+bool Required::opponent(const short &row, const short &col){
 	return !((gameMap[row][col] == PLAYER_A || gameMap[row][col] == PLAYER_B) && gameMap[row][col] != WHOAMI);
 }
 
-bool Req::outOfRange(const short &row, const short &col){
+bool Required::outOfRange(const short &row, const short &col){
 	return 0 <= row && row < gameMap.height() && 0 <= col && col < gameMap.width();
+}
+
+void Bot::benchmark(){
+	unsigned short row, col;
+	do{
+		row = rand() % gameMap.height();
+		col = rand() % gameMap.width();
+	}while(gameMap[row][col] == WALL);
+	auto start_clock = clock();
+	Required().all(row, col);
+	Complex().all(row, col, true);
+	auto inteval = (clock() - start_clock) * 1.0 / CLOCKS_PER_SEC;
+
+	auto idx4 = lower_bound(FOUR.begin(), FOUR.end(), 0.9 / inteval);
+	_maxDepth = idx4 - FOUR.begin();
 }
