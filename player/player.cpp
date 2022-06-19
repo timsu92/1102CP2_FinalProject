e1afefdac611547837ec292180117e6c1ac15533
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-06-19 22:03:35 +0800

    增加DBG資訊

--- player/player.cpp
+++ player/player.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+#define DBG
 
 // 4的指數
 const vector<unsigned long> FOUR{4, 16, 64, 256, 1024, 4096, 16384, 65536, 262144, 1048576, 4194304, 16777216, 67108864, 268435456, 1073741824, 4294967296, 17179869184, 68719476736, 274877906944, 1099511627776, 4398046511104, 17592186044416, 70368744177664, 281474976710656, 1125899906842624, 4503599627370496, 18014398509481984, 72057594037927936, 288230376151711744, 1152921504606846976, 4611686018427387904};
@@ -178,20 +179,47 @@ void Bot::benchmark(){
 
 	const auto idx4 = lower_bound(FOUR.begin(), FOUR.end(), 0.9 / average);
 	_maxDepth = min(static_cast<unsigned short>(idx4 - FOUR.begin()+1), static_cast<unsigned short>(1001 - ROUND)); // 加一是因為第一層只有當前位置，沒有計算
+#ifdef DBG
+	cerr << "[Bot::benchmark]execution costs " << average << " s, which is " << 0.9/average << " executions\n";
+	cerr << "[Bot::benchmark]running up to " << _maxDepth << " steps\n";
+#endif
 }
 
 int Bot::alpha_beta(const pair<short, short>&playerAt, const unsigned short depth, const bool isMax, const RateAndScores &parentRnS, int prunePivot) const{
 	if(depth >= _maxDepth){
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]_maxDepth reached\n";
+#endif 
 		return 0;
 	}
 	int diffRate = isMax ? INT_MIN : INT_MAX;
 	for(short dir = 0 ; dir < 4 && (isMax ? diffRate < prunePivot : diffRate > prunePivot) ; ++dir){
 		const pair<short, short>movedPlayerAt = make_pair(playerAt.first + DROW[dir], playerAt.second + DCOL[dir]);
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]player moved ";
+		switch((enum Direction)dir){
+			case UP: cerr << "up"; break;
+			case DOWN: cerr << "down"; break;
+			case LEFT: cerr << "left"; break;
+			case RIGHT: cerr << "right"; break;
+		}
+		cerr << " to (" << movedPlayerAt.first << ',' << movedPlayerAt.second << ")\n";
+#endif
 		if(!Required().all(movedPlayerAt.first, movedPlayerAt.second)){
+#ifdef DBG
+			cerr << "[Bot::alpha_beta]Required not passed\n";
+#endif 
 			continue;
 		}
 		const enum MapObjs thisObj = gameMap.at(movedPlayerAt.first).at(movedPlayerAt.second);
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]Required passed\n"
+			"[Bot::alpha_beta]thisObj is " << (char)thisObj << endl;
+#endif
 		RateAndScores thisRnS = Complex().all(movedPlayerAt.first, movedPlayerAt.second, make_pair(parentRnS.sa, parentRnS.sb), depth+1);
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]thisRnS(Complex)={" << thisRnS.r << ',' << thisRnS.sa << ',' << thisRnS.sb << "}\n";
+#endif
 		if(isMax){
 			if(thisRnS.r == INT_MIN){
 				continue;
@@ -207,7 +235,14 @@ int Bot::alpha_beta(const pair<short, short>&playerAt, const unsigned short dept
 		}
 		// 刪除這格地圖上的物件
 		gameMap[movedPlayerAt.first][movedPlayerAt.second] = PATH;
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]call alpha_beta by playerAt=(" << movedPlayerAt.first << ',' << movedPlayerAt.second << "), depth=" << depth+1 << ", " << (isMax ? "min" : "max") << ", RnS={" << (parentRnS + thisRnS).r << ',' << (parentRnS + thisRnS).sa << ',' << (parentRnS + thisRnS).sb << ", prunePivot=" << diffRate << endl;
+#endif
 		int nextRate = alpha_beta(movedPlayerAt, depth+1, !isMax, parentRnS + thisRnS, diffRate);
+#ifdef DBG
+		cerr << "[Bot::alpha_beta]back to playerAt (" << movedPlayerAt.first << ',' << movedPlayerAt.second << "), depth=" << depth << endl <<
+			"[Bot::alpha_beta]nextRate=" << nextRate << endl;
+#endif
 		// 還原這格地圖上的物件
 		gameMap[movedPlayerAt.first][movedPlayerAt.second] = thisObj;
 		if(isMax){
@@ -216,6 +251,13 @@ int Bot::alpha_beta(const pair<short, short>&playerAt, const unsigned short dept
 			}else if(nextRate == INT_MAX){
 				return INT_MAX;
 			}else{
+#ifdef DBG
+				if(thisRnS.r + nextRate > diffRate){
+					cerr << "[Bot::alpha_beta]diffRate changed to " << thisRnS.r + nextRate << endl;
+				}else{
+					cerr << "[Bot::alpha_beta]diffRate remain " << diffRate << endl;
+				}
+#endif
 				diffRate = max(diffRate, thisRnS.r + nextRate);
 			}
 		}else{
@@ -224,10 +266,20 @@ int Bot::alpha_beta(const pair<short, short>&playerAt, const unsigned short dept
 			}else if(nextRate == INT_MIN){
 				return INT_MIN;
 			}else{
+#ifdef DBG
+				if(thisRnS.r + nextRate < diffRate){
+					cerr << "[Bot::alpha_beta]diffRate changed to " << thisRnS.r + nextRate << endl;
+				}else{
+					cerr << "[Bot::alpha_beta]diffRate remain " << diffRate << endl;
+				}
+#endif
 				diffRate = min(diffRate, thisRnS.r + nextRate);
 			}
 		}
 	}
+#ifdef DBG
+	cerr << "[Bot::alpha_beta]diffRate return as " << diffRate << endl;
+#endif
 	return diffRate;
 }
 
@@ -280,19 +332,38 @@ const char* Bot::decide() const{
 	const pair<short, short> &myLocation = WHOAMI == PLAYER_A ? gameMap.playersAt.first : gameMap.playersAt.second;
 	for(short dir = 0 ; dir < 4 ; ++dir){
 		const pair<short, short> movedPlayerAt = make_pair(myLocation.first + DROW[dir], myLocation.second + DCOL[dir]);
+#ifdef DBG
+		cerr << "[Bot::decide]player moved to (" << movedPlayerAt.first << ',' << movedPlayerAt.second << ")\n";
+#endif
 		if(!Required().all(movedPlayerAt.first, movedPlayerAt.second)){
+#ifdef DBG
+			cerr << "[Bot::decide]Required not passed";
+#endif
 			continue;
 		}
 		const enum MapObjs thisObj = gameMap[movedPlayerAt.first][movedPlayerAt.second];
+#ifdef DBG
+		cerr << "[Bot::decide]Required passed\n"
+			"[Bot::decide]thisObj is " << (char)thisObj << endl;
+#endif
 		int thisRate = Complex().all(movedPlayerAt.first, movedPlayerAt.second, SCORE, 1).r;
+#ifdef DBG
+		cerr << "[Bot::decide]thisRate=" << thisRate << endl;
+#endif
 		if(thisRate == INT_MIN){continue;}
 		else if(thisRate == INT_MAX){
 			maxDirIdx = dir;
 			break;
 		}
 		gameMap[movedPlayerAt.first][movedPlayerAt.second] = PATH;
+#ifdef DBG
+		cerr << "[Bot::decide]call alpha_beta by playerAt (" << movedPlayerAt.first << ',' << movedPlayerAt.second << "), depth=1, min, RnS={0," << SCORE.first << ',' << SCORE.second << "}, prunePivot=" << maxRate << endl;
+#endif
 		int nextRate = alpha_beta(movedPlayerAt, 1, false, RateAndScores{0, SCORE.first, SCORE.second}, maxRate);
 		gameMap[movedPlayerAt.first][movedPlayerAt.second] = thisObj;
+#ifdef DBG
+		cerr << "[Bot::decide]nextRate=" << nextRate << endl;
+#endif
 		if(nextRate == INT_MIN){continue;}
 		else if(nextRate == INT_MAX){
 			maxDirIdx = dir;
