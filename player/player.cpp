3f85b906bd46f3b5080b188b687459126a593f26
timsu92 <33785401+timsu92@users.noreply.github.com>
2022-06-21 19:24:14 +0800

    [Complex::corner2Center]分數改為0.13

--- player/player.cpp
+++ player/player.cpp
@@ -383,7 +383,7 @@ inline bool Required(const short &row, const short &col){
 }
 
 struct RateAndScores Complex::corner2Center(const pair<short, short> &playerAt, const pair<short, short> &movedPlayerAt, const pair<int, int> &scores, const unsigned short depth){
-	struct RateAndScores nearRnS = {0.1, scores.first, scores.second}, farRnS = {0, scores.first, scores.second};
+	struct RateAndScores nearRnS = {0.13, scores.first, scores.second}, farRnS = {0, scores.first, scores.second};
 	if(gameMap.myLocation.first < gameMap.height() * 0.2 || gameMap.myLocation.first > gameMap.height() * 0.7){
 		if(pow(movedPlayerAt.first - gameMap.height()/2, 2) + pow(movedPlayerAt.second - gameMap.width()/2, 2) <
 				pow(gameMap.myLocation.first - gameMap.height()/2, 2) + pow(gameMap.myLocation.second - gameMap.width()/2, 2)){
